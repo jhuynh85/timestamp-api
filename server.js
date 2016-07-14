c7907757f50fd22d19dee34a011cf13523664dfd
Joseph Huynh <joe.huynh@gmail.com>
2016-07-14 00:24:00 +0000

    Added unix timestamp and natural language date handling

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
+var moment = require('moment'); // Moment.js for parsing/displaying date
 var express = require('express');
 var app = express();
+var date;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/:timestamp', function (req, res) {
+    var unix = null;
+    var natural = null;
+    var time = req.params.timestamp;
+    
+    // Check if argument is natural language date or unix timestamp
+    if (isNaN(time)) {
+        date = moment(new Date(time));
+    } else {
+        date = moment.unix(parseInt(time));
+    }
+    
+    if (date.isValid()){
+        unix = date.unix();
+        natural = date.format('MMMM D, YYYY');
+    }
+    
+    res.send({
+        "unix": unix,
+        "natural": natural
+    });
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(8080);
